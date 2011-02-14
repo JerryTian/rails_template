a50965b1e25a898f6e943f527f54da9243b91771
Jerry Tian <jerry.tian@suite101.com>
2011-02-14 09:59:16 -0800

    Template for all other suite101 rails project

---
+++ Gemfile
@@ -0,0 +1,16 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.4'
+gem 'sqlite3'
+gem 'haml'
+
+group :development do
+	gem 'ruby-debug'
+	gem "nifty-generators"
+end
+
+group :test do
+	gem 'cucumber-rails'
+	gem 'capybara'
+	gem 'database_cleaner'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo::Application.config.secret_token = '0eb27159902dffd55cc87f545fc0a4f120fcc670b57c9998979e597002e708722deb5030dc8bbb000d72f37fa85ca482118f1712064dfedf260769ef34099395'
